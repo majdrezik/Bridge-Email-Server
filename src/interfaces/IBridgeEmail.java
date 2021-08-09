2f2269836538d609dbe455b6e1f5bfbadd0a9e78
Majd Rezik <majdrezik@gmail.com>
2021-08-10 02:06:52 +0300

    added documentation to the Interface

--- src/interfaces/IBridgeEmail.java
+++ src/interfaces/IBridgeEmail.java
@@ -5,8 +5,34 @@ import java.util.Properties;
 import model.Email;
 
 public interface IBridgeEmail {
+	/**
+	 * setAttributes receives email from the employee and saves it in the Bridge Server.
+	 * And checks whether the email is valid or not.
+	 * @param email
+	 * @throws Exception
+	 */
 	public void setAttributes(Email email) throws Exception;
+	
+	/**
+	 * identifyVendor identifies the vendor by the postfix of the given email.
+	 * And tries to connect to the appropriate server.
+	 * @param email
+	 * @throws Exception
+	 */
 	public void identifyVendor(String email) throws Exception;
+	
+	/**
+	 * connect imitates connecting to the server by sleeping 2 seconds.
+	 * And calls sendEmail to send the email after connecting to the server.
+	 * @param properties
+	 * @throws Exception
+	 */
 	public void connect(Properties properties) throws Exception;
+	
+	/**
+	 * sendEmail imitates sending a real email by sleeping 3 seconds and at the end prints the email.
+	 * @param email
+	 * @throws Exception
+	 */
 	public void sendEmail(Email email) throws Exception;
 }
